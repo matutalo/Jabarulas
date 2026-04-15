251b701e84dd8485f93e927f4cad1f337e902782
matutalo <fabianaguntert@gmail.com>
2026-04-15 18:03:46 +0000

    Co-authored-by: igorbdl523 <igorbdl523@users.noreply.github.com>

--- Jabarulas/jogo.ts
+++ Jabarulas/jogo.ts
@@ -1,21 +1,57 @@
-class Personagem{
+
+class Personagem {
+
     nome: string;
     classe: string;
-    vida: number;
-    constructor(nome: string, classe: string, vida: number) {
-       this.nome = nome;
-       this.classe = classe;
-       this.vida = vida;
+    HP: number;
+    MP: number;
+    forca: number;
+    agilidade: number;
+    constructor(nome: string, classe: string, HP: number, MP: number, forca: number, agilidade: number) {
+        this.nome = nome;
+        this.classe = classe;
+        this.HP = HP;
+        this.MP = MP;
+        this.forca = forca;
+        this.agilidade = agilidade;
     }
-    tomarDano() {
 
+     atacar(alvo: Inimigo) {
+        console.log(`${this.nome} ataca ${alvo.nome}!`);
+        const dano = this.forca;
+        alvo.sofrerDano(dano);
     }
-    recuperarVida() {
-            this.vida += (Math.random() * 20);
-    }
+
+    sofrerDano(quantidade: number) {
+        quantidade = 2 * (this.forca * 0.5);
+        this.HP -= quantidade;
+        console.log(`${this.nome} perdeu ${quantidade} de HP. HP atual: ${this.HP}`);
+        
+        if (this.HP <= 0) {
+            console.log(`${this.nome} foi derrotado!`);
+        }
+}
+}
+//classe inimigo
+class Inimigo extends Personagem {
+    constructor(){
+     super(
+     "monstro",
+     classes[Math.random() * 3],
+     Math.floor(Math.random() * 50) + 10, // HP aleatório
+     10,
+     10, 
+     1
+    )
+}
 }
 
-const pTeste = new Personagem('Shrek', 'Ogro', 10);
 
-console.log(Math.random() * 20)
-console.log(pTeste.recuperarVida())
+const classes = ["mago", "guerreiro", "assassino", "tanque"];
+
+
+
+const persoangemTeste = new Personagem('Aurélio', 'mago', 50, 30., 10, 10);
+const inimigoTeste = new Inimigo();
+
+
